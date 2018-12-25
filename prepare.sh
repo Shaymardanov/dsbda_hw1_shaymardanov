99c10e91bcaeec73a5721a96e01e9ff7c729732e
Timur Shaymardanov <t.shaymardanov@gmail.com>
2018-12-25 22:24:25 +0300

    New Commit

--- prepare.sh
+++ prepare.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-hadoop fs -mkdir /lab1
-hadoop fs -mkdir /lab1/input1
-hadoop fs -mkdir /lab1/input2
+hadoop fs -mkdir /hw1
+hadoop fs -mkdir /hw1/input1
+hadoop fs -mkdir /hw1/input2
 
-hadoop fs -rm /lab1/input1/*
-hadoop fs -rm /lab1/input2/*
+hadoop fs -rm /hw1/input1/*
+hadoop fs -rm /hw1/input2/*
 
-hadoop fs -put ./storage/log1.txt /lab1/input1
-hadoop fs -put ./storage/ip.txt   /lab1/input2
+hadoop fs -put ./storage/log1.txt /hw1/input1
+hadoop fs -put ./storage/ip.txt   /hw1/input2
